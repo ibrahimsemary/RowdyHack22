47ee2cc470fbce03d294c80254ae4b27d88c6233
Howiemorg <morgenthalerhowie@yahoo.com>
2022-03-27 01:21:05 -0500

    m

--- ebayParse.js
+++ ebayParse.js
@@ -3,7 +3,7 @@ function ebayParse(search){
     results = [[]];
     let request = new XMLHttpRequest();
     let ebayBase = ""
-    let amazonBase = ''
+    // let amazonBase = ''
 
     url = ebayBase + search;
     request.open("GET",url);
@@ -23,16 +23,16 @@ function ebayParse(search){
         
     }
 
-    url = amazonBase + search;
-    request.open("GET",url);
-    request.send();
-    request.onload = () => {
-        console.log(request);
-        myJSON = JSON.parse(request.response);
-        values = myJSON["search_results"];
-        for(let i = 0; i < myJSON.length && i < 16; i++){
-            results.push([values[i]["title"],values[i]["prices"]["values"],"New",values[i]["image"],values[i]["link"]]);
-        }
+    // url = amazonBase + search;
+    // request.open("GET",url);
+    // request.send();
+    // request.onload = () => {
+    //     console.log(request);
+    //     myJSON = JSON.parse(request.response);
+    //     values = myJSON["search_results"];
+    //     for(let i = 0; i < myJSON.length && i < 16; i++){
+    //         results.push([values[i]["title"],values[i]["prices"]["values"],"New",values[i]["image"],values[i]["link"]]);
+    //     }
         
-    }
+    // }
 }
