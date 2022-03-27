33a84a1447f65fa0878634cda8e9c14e59d28bbc
ibrahimsemary <ibrahimsemary@tamu.edu>
2022-03-27 00:37:55 -0500

    howdy

---
+++ main.js
@@ -0,0 +1,9 @@
+let amount = "";
+
+document.getElementById("dark_field").oninput = function() {
+    amount = document.getElementById("dark_field").value;
+}
+
+document.getElementById("searchButton").onclick = function () {
+    console.log("clicked");
+}
