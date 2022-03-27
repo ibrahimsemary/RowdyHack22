95de74d7880cd2feb45e3953a8299ca328573321
Howiemorg <morgenthalerhowie@yahoo.com>
2022-03-27 01:00:18 -0500

    m

--- ebayParse.js
+++ ebayParse.js
@@ -1,24 +1,17 @@
 
-results = [[]];
-let request = new XMLHttpRequest();
-
-search = "users";
-
-url = "https://jsonplaceholder.typicode.com/" + search;
-request.open("GET",url);
-request.send();
-request.onload = () => {
-    console.log(request);
-    // if(request.status === 200){
-    //     console.log(JSON.parse(request.response));
-    // }
-    // else{
-    //     console.log('error $(request.status) $(request.statusText)');
-    // }
-    myJSON = JSON.parse(request.response);
-    values = myJSON["search_results"];
-    for(let i = 0; i < myJSON.length; i++){
-        results.push([values[i]["title"],values[i]["prices"],values[i]["condition"],values[i]["image"],values[i]["link"]])
+function ebayParse(search,base){
+    results = [[]];
+    let request = new XMLHttpRequest();
+    url = base + search;
+    request.open("GET",url);
+    request.send();
+    request.onload = () => {
+        console.log(request);
+        myJSON = JSON.parse(request.response);
+        values = myJSON["search_results"];
+        for(let i = 0; i < myJSON.length; i++){
+            results.push([values[i]["title"],values[i]["prices"],values[i]["condition"],values[i]["image"],values[i]["link"]])
+        }
+        return results;
     }
-    console.log(results);
 }
